50642a579cbee6bf8f97618f85bca1c47d403c4d
sergeybee <raifa@list.ru>
2022-11-01 14:35:18 +0300

    Rename folder and path in config.py file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 # Данные для личного кабинета сотрудника
 MAIN_LINK = "http://creditcalculator.pointschool.ru/login"
-JSON_FILE_PATH = "TestData/DataSet.json"
+JSON_FILE_PATH = "data/DataSet.json"
 LOGIN = ""
 PASS = ""
